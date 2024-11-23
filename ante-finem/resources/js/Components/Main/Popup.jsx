3da8401095293e9966da4b5e169727d653b6ed9c
SzymonDrzazga <szydrzazga@gmail.com>
2024-11-23 10:28:58 +0100

    hotfix

--- ante-finem/resources/js/Components/Main/Popup.jsx
+++ ante-finem/resources/js/Components/Main/Popup.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './Popup.css';
+import React from "react";
+import "./Popup.css";
 
 const Popup = ({ title, content, img_popup, onClose }) => {
-  return (
-    <div className="popup-overlay">
-      <div className="popup-content">
-      {img_popup && <img
-                                    src={img_popup}
-                                    className="rounded-full h-[5vh] w-[2vw] img_popup"
-                                />}
-        <h2><b>{title}</b></h2>
-        <p>{content}</p>
+	return (
+		<div className="popup-overlay">
+			<div className="popup-content">
+				{img_popup && <img src={img_popup} className="rounded-full h-[5vh] w-[2vw] img_popup" />}
+				<h2>
+					<b>{title}</b>
+				</h2>
+				<p>{content}</p>
 
-        <button onClick={onClose}>Close</button>
-      </div>
-    </div>
-  );
+				<button onClick={onClose}>Close</button>
+			</div>
+		</div>
+	);
 };
 
 export default Popup;
